561c039f5db291ed7c838e06b48b7466e2a618eb
divyank31 <68773301+divyank31@users.noreply.github.com>
2021-09-15 17:20:23 +0530

    Update google-serach-lambdatest.py

--- google-serach-lambdatest.py
+++ google-serach-lambdatest.py
@@ -1,103 +1,239 @@
-import unittest
+#!/usr/local/bin/python
+# -*- coding: utf-8 -*-
 import time
-import os
-from selenium import webdriver
+import multiprocessing
 from selenium.webdriver.common.keys import Keys
+from selenium.webdriver.common.by import By
+#from browserstack.local import Local
+import urllib3
+import sys
+import threading
+from selenium import webdriver
+from selenium.webdriver.chrome.options import Options as ChromeOptions
+from selenium.webdriver.chrome.options import Options
 
-
-class LTAutomate(unittest.TestCase):
-    """
-    LambdaTest selenium automation sample example
-    Configuration
-    ----------
-    username: Username can be found at automation dashboard
-    accessToken:  AccessToken can be genarated from automation dashboard or profile section
-    Result
-    -------
-    Execute Test on lambdatest Distributed Grid perform selenium automation based 
-    """
-
+def call():
+    platform = "desktop"
     
-    def setUp(self):
-        """
-        Setup remote driver
-        Params
-        ----------
-        platfrom : Supported platfrom - (Windows 10, Windows 8.1, Windows 8, Windows 7,  macOS High Sierra, macOS Sierra, OS X El Capitan, OS X Yosemite, OS X Mavericks)
-        browserName : Supported platfrom - (chrome, firefox, Internet Explorer, MicrosoftEdge)
-        version :  Supported list of version can be found at https://www.lambdatest.com/capabilities-generator/
-        Result
-        -------
-        """
-        # username: Username can be found at automation dashboard
-        username= os.getenv('LT_USERNAME')  
-
-        # accessToken:  AccessToken can be genarated from automation dashboard or profile section
-        accessToken= os.getenv('LT_ACCESS_KEY')  
-        # gridUrl: gridUrl can be found at automation dashboard
-        gridUrl = os.getenv('LT_GRID_URL')
-        # get tunnel name
-        lambda_tunnel= False
-        if os.getenv('LT_TUNNEL_NAME') is not None: 
-           lambda_tunnel=True 
-        
+    if platform=="desktop":
         desired_cap = {
-            'platform' : "win10", 
-            'browserName' : "chrome",
-            'version' :  "67.0",
-            # Resolution of machine
-            "resolution": "1024x768", 
-            "name": "Azure pipeline extension google search test ",
-            "build": "Azure pipeline extension",
-            "network": True,
-            "video": True,
-            "visual": True,
+            "browserName":"Chrome",      #Chrome, MicrosoftEdge
             "console": True,
-            "tunnel":lambda_tunnel,
+            "build": "diver and selenium",
+            "name" : "Test 1",
+            "version":"latest",                   #88.0
+            "enableNetworkThrottling":False,
+            #"forceUpdate":True,
+            "headless":False,
+            "network": False,
+            #"performance":False,
+            "platform": "Windows 10",       #MacOS Big sur, Windows 10
+            "platformName": "Windows 10",
+            "resolution":"1024x768", 
+            #"selenium_version":"4.0.0-beta-3",
+            #"tunnel": True,
+            #"tunnelIdentifier":"",
+            #"fixedIP": "10.80.16.19",                          #"10.81.97.228"
+            "video":True,
+            'goog:chromeOptions': {'extensions': [], 'args': ['--log-level=3']},
+            "visual":True,
+            "w3c":True
         }
+    #    desired_cap = {
+    #         "acceptInsecureCerts":True,
+    #         "acceptSslCerts":True,
+    #         "browserName":"safari",
+    #         "build":"React Wayforward App Smoke Tests Bundle Fixed IP",
+    #         "console":"true",
+    #         "enableNetworkThrottling":False,
+    #         "extendedDebuging":True,
+    #         "headless":False,
+    #         "idleTimeout":"180",
+    #         "loggingPrefs":{
+    #             "browser":"ALL",
+    #             "driver":"ALL",
+    #             "server":"ALL"
+    #         },
+    #         "name":"React Wayforward App test_signup_wld_sso cascade",
+    #         "network":True,
+    #         "network.full.har":True,
+    #         "network.har":True,
+    #         "performance":False,
+    #         "platform":"macos 11.0",
+    #         "resolution":"1280x1024",
+    #         "safari.cookies":True,
+    #         #"tunnel":True,
+    #         "version":"latest.0",
+    #         "video":True,
+    #         "visual":False,
+    #         "w3c":True,
+    #         "fixedIP": "10.242.32.62", #10.242.32.146, 10.242.32.152, 10.242.32.147, 10.242.32.62
+    #     }
+    elif platform=="mobile":
+        desired_cap = { 
+            "build" : "iOS device testing 14.5",
+            "name" : "Test",    # 
+            "platformName" : "iOS",
+            "deviceName" : "iPhone 12", # 
+            "platformVersion":"14.4",
+            "fixedIP":"10.92.181.43",          #23.105.12.40(Android), (10.242.32.103)ios  (23.105.12.36)android prod
+            "appiumVersion":"1.21.0",
+            #"fixedPort":"8000",
+            #"console": "warn",
+            #"network": True,
+            #"browserName" : "Chrome",
+            #"tunnel": True,
+            #'isRealMobile' : True,
+            #"orientation": "",
+            #"deviceOrientation": "",
+            #"chromedriverArgs": "disable-web-security",
+        }
+    #STAGE 
+    # url = "https://divyanks:i1dGEHcw7jov92lkSz1pajKv4XLFde4Jd6IgMiUGZFKoaRAq8r@stage-hub.lambdatest.com/wd/hub"
+    #STAGE FREEMIUM
+    #url = "https://divyank-1:pOR8MLXkVNJySDQFRgeGT3RoTMVSUcebv4MCmwAILnY7WQVKSO@stage-hub.lambdatest.com/wd/hub"
+    #PRODUCTION
+    url = "https://divyanks:XFSP1IqZMWTXUC1iwbVqFCClLX654vuFg8OZUMyCfwbfygavq1@hub.lambdatest.com/wd/hub"
+    #url = "https://divyanks:XFSP1IqZMWTXUC1iwbVqFCClLX654vuFg8OZUMyCfwbfygavq1@hub-ohio.lambdatest.com/wd/hub"
+    #PROD FREEMIUM
+    #url = "https://divyank-1:24yC7K6WqX4oQztJJNQub5mJzMd3vg5qo9IC0Oe2t8wwZn5jFg@hub-ohio.lambdatest.com/wd/hub"
+    #PROD BROWSERSTACK
+    #url = "https://falconmagicleap_vci1oa:JxzszpGVzPndo6ySxp3r@hub-cloud.browserstack.com/wd/hub"
+    #url = "https://divyank31:QAN5BKWJaP7zh5p5yalMBM5jRz4AA094G0osRrbShUGIvpnF4v@hub.lambdatest.com/wd/hub"
+    #DEV ENV
+    #url = "https://divyanks:i1dGEHcw7jov92lkSz1pajKv4XLFde4Jd6IgMiUGZFKoaRAq8r@hub.divyank-commandlogs.dev.lambdatest.io/wd/hub"
+    #url = "https://divyanks:i1dGEHcw7jov92lkSz1pajKv4XLFde4Jd6IgMiUGZFKoaRAq8r@hub.divyank-14-sep.dev.lambdatest.io/wd/hub"
+    
+    #BrowserStack
+    #url = "https://divyanksingh1:CNnn3B4jqfsEFKbcJYyp@hub-cloud.browserstack.com/wd/hub"   #LT
+    #url = "https://divyanksingh_mwxB5t:37ZZgnXZksNUysXFSyYk@hub-cloud.browserstack.com/wd/hub"      #personal
+    #webhook
+    #url = "https://webhook.site/1c1fb21c-5fcb-4dcc-9ea5-920e8e28b852"
 
-        # URL: https://{username}:{accessToken}@beta-hub.lambdatest.com/wd/hub
-        url =  "https://"+username+":"+accessToken+"@"+gridUrl 
-        
-        print("Initiating remote driver on platfrom: "+desired_cap["platform"]+" browser: "+desired_cap["browserName"]+" version: "+desired_cap["version"])
-        print(url)
-        print("lambdaTunnel :"+str(lambda_tunnel))
-        self.driver = webdriver.Remote(
-            desired_capabilities=desired_cap,
-            command_executor= url
-        )
+    # #creates an instance of Local
+    # bs_local = Local()
+
+    # bs_local_args = { "key": "37ZZgnXZksNUysXFSyYk" }
+
+    # #starts the Local instance with the required arguments
+    # bs_local.start(**bs_local_args)
+
+    # #check if BrowserStack local instance is running
+    # print(bs_local.isRunning())
+
+
+    # print(url)
+    # start_time = time.time()
+    # print(desired_cap)
 
     
-    def test_search_in_google(self):
-        """
-        Setup remote driver
-        Params
-        ----------
-        Execute test:  navigate google.com search LambdaTest
-        Result
-        -------
-        print title
-        """
-        driver = self.driver
-        print("Driver initiated sucessfully.  Navigate url")
-        driver.get("https://www.google.com/ncr")
-
-        print("Searching lambdatest on google.com ")
-        time.sleep(8)
-        elem = driver.find_element_by_name("q")
-        elem.send_keys("lambdatest.com")
-        elem.submit()
-
-        print("Printing title of current page :"+driver.title)
-        driver.execute_script("lambda-status=passed")
-        print("Requesting to mark test : pass")
+    # chrome_options = ChromeOptions()
+    # # chrome_options.add_extension("/Users/applemd1011/Downloads/LambdaDemo/chrome.zip")
+    # chrome_options.add_argument("--log-level=3")
+    # create new Chrome driver object with Chrome extension
+    # driver = webdriver.Chrome(chrome_options=chop)
 
     
-    def tearDown(self):
-        """
-        Quit selenium driver
-        """
-        self.driver.quit()
+    
+    # desired_cap.update(chrome_options.to_capabilities())
+    # print(desired_cap)
+    driver=webdriver.Remote(desired_capabilities=desired_cap,command_executor=url)
+    
+    # print("setup time {0} secs".format((time.time() - start_time) % 60))
+    #driver.get("http://localhost:3000/")
+    #time.sleep(5)
+    print(driver.session_id)
+    driver.get("https://mylocation.org/")
+    driver.get("https://www.whatismybrowser.com/")
+    #time.sleep(10)
+    print("=============1===========")
+    driver.get("https://www.google.com/")
+    #time.sleep(60)
+    driver.get("https://www.halowaypoint.com/en-us/games/halo-infinite")
+    print("=============2===========")
+    #time.sleep(60)
+    driver.get("https://www.serverless.com/blog/cors-api-gateway-survival-guide")
+    # driver.implicitly_wait(20)
+    try :
+        driver.find_element_by_id("divyank")
+    except :
+        pass
+    #time.sleep(30)
+    driver.get("https://en.wikipedia.org/wiki/Halo_Infinite")
+    print("=============3===========")
+    #time.sleep(30)
+    driver.get("https://www.thrillophilia.com/best-mountain-trek-in-india")
+    #time.sleep(60)
+    driver.get("https://indiahikes.com/")
+    print("=============4===========")
+    driver.get("https://google.com/")
+    #time.sleep(150)
+    #driver.delete_all_cookies()
+    #driver.clear_session_storage()
+    #Tunnel
+    # driver.get("http://localhost:8888/")
+    # driver.get("http://localhost:8888/Test Scripts/")
+    # driver.get("http://localhost:8888/Test Scripts/hashing/")
+    # driver.get("http://localhost:8888/Test Scripts/local/")
+    # driver.get("http://localhost:8888/Test Scripts/local/kafka/")
+    # driver.get("http://localhost:8888/Go-Projects/")
+    # driver.get("http://localhost:8888/Go-Projects/logging/")
 
-if __name__ == "__main__":
-    unittest.main()
+
+    #Web Sockets
+    # driver.get("https://www.websocket.org/echo.html")
+    # print("=============1===========")
+    # driver.find_element_by_id("connect").send_keys(Keys.ENTER)
+    # print("=============2===========")
+    # driver.find_element_by_id("send").send_keys(Keys.ENTER)
+    # print("=============3===========")
+    # driver.find_element_by_id("sendMessage").send_keys("Testing Web Sockets!!!")
+    # print("=============4===========")
+    # driver.find_element_by_id("send").send_keys(Keys.ENTER)
+    # print("=============5===========")
+
+    #driver.find_element_by_id("send").send_keys(Keys.ENTER)
+
+    # driver.get("https://www.python.org/")
+    # #driver.findElement(By.id("copy_to_clip")).click();
+    # element = driver.find_element_by_xpath("//*[@id=\"dive-into-python\"]/ul[2]/li[1]/div[2]/h1")
+    # element.send_keys(Keys.CONTROL + "a")
+    # driver.implicitly_wait(3)
+    # element1 = driver.find_element_by_xpath("//*[@id=\"dive-into-python\"]/ul[2]/li[4]/div[1]/pre/code")
+    # element1.send_keys(Keys.CONTROL + "c")
+    # element2 = driver.find_element(By.name("q"))
+    # element2.send_keys(Keys.CONTROL + "v")
+    # driver.implicitly_wait(3)
+
+
+    #driver.execute_script("lambda-status=failed")
+    #time.sleep(60)
+    # driver.get("https://www.trekbikes.com/in/en_IN/bikes/mountain-bikes/c/B300/")
+    # #time.sleep(60)
+    # driver.get("https://www.lonelyplanet.com/articles/best-treks-in-the-world")
+    # #time.sleep(60)
+    # driver.get("https://www.adventurenation.com/activity/trekking")
+    # #time.sleep(60)
+    # driver.get("https://www.intrepidtravel.com/adventures/trekking-training-guide-tips/")
+    # time.sleep(60)
+    # print(driver.execute_script("lambda-perform-keyboard-events:^p")) #for CTRL+P
+    # time.sleep(5)
+    # print(driver.execute_script("lambda-perform-keyboard-events:{ENTER}")) #pressing ENTER
+    # time.sleep(2)
+    # print(driver.execute_script("lambda-perform-keyboard-events:demo-testing-keystroke.pdf"))  #inserting charcters
+    # time.sleep(2)
+    # print(driver.execute_script("lambda-perform-keyboard-events:{ENTER}"))
+
+    start_time = time.time()
+    driver.quit()
+    print("quit time {0} secs".format((time.time()-start_time) % 60))
+    #stop the Local instance
+    #bs_local.stop()
+
+if __name__ == '__main__':
+    jobs = []
+    for i in range(1):
+        p = multiprocessing.Process(target=call)
+        jobs.append(p)
+        p.start()
+
