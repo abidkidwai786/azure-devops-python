c3b05f798728f17d9f1c7443800b362592a16de2
divyank31 <divyanks@lambdatest.com>
2021-09-16 19:56:38 +0530

    exception handling

--- google-serach-lambdatest.py
+++ google-serach-lambdatest.py
@@ -53,7 +53,7 @@ def call():
 
 if __name__ == '__main__':
     jobs = []
-    for i in range(60):
+    for i in range(200):
         p = multiprocessing.Process(target=call)
         jobs.append(p)
         p.start()
