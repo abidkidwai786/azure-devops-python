d7097e08bde32af4613bc0d1027bce7d0f872c5d
divyank31 <divyanks@lambdatest.com>
2021-09-16 19:31:10 +0530

    exception handling

--- google-serach-lambdatest.py
+++ google-serach-lambdatest.py
@@ -15,7 +15,7 @@ def call():
     desired_cap = {
         "browserName":"Chrome",
         "console": True,
-        "build": "Idle Timeout issue-7",
+        "build": "Idle Timeout issue-8",
         "name" : "Test 1",
         "version":"latest",                   #88.0
         "headless":False,
@@ -52,7 +52,7 @@ def call():
 
 if __name__ == '__main__':
     jobs = []
-    for i in range(30):
+    for i in range(50):
         p = multiprocessing.Process(target=call)
         jobs.append(p)
         p.start()
